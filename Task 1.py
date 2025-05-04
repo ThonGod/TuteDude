88856f26143bdc66784e71076483faa9bcc0f155
ThonGod <theengineerpoet09@gmail.com>
2025-05-04 21:29:22 +0530

    Add files via upload

---
+++ Task 1.py
@@ -0,0 +1,12 @@
+a=input("Enter the first nomber: ")
+b=input("Enter the second nomber: ")
+a=int(a)
+b=int(b)
+s=a + b
+d=a - b
+m=a*b
+div=a/b
+print("Addition: " ,s)
+print("Subtraction: ",d)
+print("Multiplication: ",m)
+print("Division: ",div)
---
+++ Task 2.py
@@ -0,0 +1,6 @@
+f=input("Enter your first name:")
+s=input("Enter your last name:")
+f=str(f)
+s=str(s)
+full=f + " " + s
+print("Hello, ", full,"! Welcome to the Python program")
